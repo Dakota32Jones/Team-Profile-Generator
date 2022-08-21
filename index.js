0ec613d2c8073fb3c8534333f103a3402770456c
Dakota Jones <dakota32jones@gmail.com>
2022-08-21 17:20:18 -0500

    got application running from command line and displaying cards dynamically

--- index.js
+++ index.js
@@ -7,104 +7,78 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const html = require("./src/htmlTemp");
-const validator = require("email-validator");
-
-// adding asynchronous functions
 
 const writeFileAsync = util.promisify(fs.writeFile);
 const appendFileAsync = util.promisify(fs.appendFile);
 
 let teamArray = [];
-let teamString = ``;
+let teamstr = ``;
 
-console.clear();
-console.log("--------------------");
-console.log("Team Portfolio Generator");
-
-// Functions to run application
-
+//this calls all the functions in one in order
 async function main() {
   try {
     await prompt();
+    // for i to teamArray.length  =>
 
     for (let i = 0; i < teamArray.length; i++) {
-      teamString = teamString + html.generateCard(teamArray[i]);
+      //template literal=``
+      teamstr = teamstr + html.generateCard(teamArray[i]);
     }
 
-    let finalHtml = html.generateHTML(teamString);
+    let finalHTML = html.generateHTML(teamstr);
 
-    console.clear();
-    console.log("---------------");
-    console.log("Generating index.html file.....");
+    console.log(teamstr);
 
-    writeFileAsync("./dist/index.html", finalHtml);
+    //call generate function to generate the html template literal
 
-    console.clear();
-    console.log("-----------------");
-    console.log("index.html file created successfully");
-    console.log("----------------------");
+    //write file
+    writeFileAsync("./dist/index.html", finalHTML);
   } catch (err) {
     return console.log(err);
   }
 }
 
-// adding inquirer prompts to collect the data from user when using application
-
 async function prompt() {
   let responseDone = "";
-
+  // prompt to collect input and use do while atleast one and do it number of times depending on the while condition
   do {
     try {
-      console.log("----------------");
-      let response = await inquirer.prompt([
+      response = await inquirer.prompt([
         {
           type: "input",
           name: "name",
-          message: "What is the employee's Name?",
-          validate: function validateName(name) {
-            return name !== "";
-          },
+          message: "What is the employee's name?: ",
         },
         {
           type: "input",
-          name: "name",
-          message: "Please enter Employee ID: ",
-          validate: function validateName(name) {
-            return name !== "";
-          },
+          name: "id",
+          message: "Enter the employee's ID: ",
         },
         {
           type: "input",
-          name: "name",
-          message: "Please enter the employee's email address: ",
-          // have to validate using email-validator
-          validate: function validateName(name) {
-            return validator.validate(name);
-          },
+          name: "email",
+          message: "Enter the employee's email address: ",
         },
         {
           type: "list",
-          name: "name",
-          message: "Please enter the employee's role: ",
+          name: "role",
+          message: "What what is the employee's role:",
           choices: ["Engineer", "Intern", "Manager"],
         },
       ]);
 
       let response2 = "";
+      // if else statement
 
       if (response.role === "Engineer") {
         response2 = await inquirer.prompt([
           {
             type: "input",
             name: "x",
-            message: "What is the employees github username?: ",
-            validate: function validateName(name) {
-              return name !== "";
-            },
+            message: "What is the employee's github username?:",
           },
         ]);
-        //   add to team array
-
+        //store the object and push
         const engineer = new Engineer(
           response.name,
           response.id,
@@ -112,39 +86,17 @@ async function prompt() {
           response2.x
         );
         teamArray.push(engineer);
-      } else if (response.role === "Manager") {
-        response2 = await inquirer.prompt([
-          {
-            type: "input",
-            name: "x",
-            message: "What is the employee's office number?: ",
-            validate: function validateName(name) {
-              return name !== "";
-            },
-          },
-        ]);
-
-        // add to team array
-        const manager = new Manager(
-          response.name,
-          response.id,
-          response.email,
-          response2.x
-        );
-        teamArray.push(manager);
       } else if (response.role === "Intern") {
         response2 = await inquirer.prompt([
           {
             type: "input",
+
+            //the x is to only store into the team array
             name: "x",
-            message: "What school is the employee attending?: ",
-            validate: function validateName(name) {
-              return name !== "";
-            },
+            message: "What school is the employee attending?:",
           },
         ]);
-
-        // add to team array
+        //store the object and push
         const intern = new Intern(
           response.name,
           response.id,
@@ -152,21 +104,42 @@ async function prompt() {
           response2.x
         );
         teamArray.push(intern);
+      } else if (response.role === "Manager") {
+        response2 = await inquirer.prompt([
+          {
+            type: "input",
+            name: "x",
+            message: "What is the employee's office number?:",
+          },
+        ]);
+        //store the object and push
+        const manager = new Manager(
+          response.name,
+          response.id,
+          response.email,
+          response2.x
+        );
+        teamArray.push(manager);
       }
     } catch (err) {
       return console.log(err);
     }
+    console.log(teamArray);
+    //need to prompt do you want to continue
+
     responseDone = await inquirer.prompt([
       {
         type: "list",
         name: "finish",
-        message: "Would you like to continue?: ",
+        message: "Do you want to continue?: ",
         choices: ["Yes", "No"],
       },
     ]);
+
+    // console.log(responseDone.choices);
+    //the while parameter is saying continue running the code if the user selects "yes"
   } while (responseDone.finish === "Yes");
 }
 
-// start and run program
-
+//call function to run application on the server
 main();
--- src/htmlTemp.js
+++ src/htmlTemp.js
@@ -1,68 +1,86 @@
-const generateHTML = function (teamString) {
+const generateHTML = function (teamstr) {
+  console.log("Inside gen html");
   return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Team Portfolio</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-    </style>
-    </head>
-    <body>
-    <div class="header">
-    <div class="jumbotron bg-warning">
-       <h1 class="display-4 text-white text-center">My IT Team</h1>
-    </div>
-    </div>
-    <div class="container-body container-fluid">
-       <div class="row">
-            ${teamString} 
-        </div>
-    </div>
-    <script src="https://kit.fontawesome.com/257de25400.js" crossorigin="anonymous"></script>  
-    </body>
-    </html>`;
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>TPG</title>
+<link href="https://fonts.googleapis.com/css?family=Bungee+Inline|Bungee+Shade&display=swap" rel="stylesheet">
+<style>
+ body {
+     margin: 0%;
+     height: 100vh;
+ }
+ .header {
+     background-color: rgb(245, 247, 155);
+     border: solid black;
+     text-align: center;
+     font-size: 30px;
+     font-family: 'Bungee Shade', cursive;  
+ }
+ .container-body {
+     display: flex;
+     justify-content: space-evenly;
+ }
+ .card {
+     background-color:rgb(255, 255, 255);
+     margin-top: 4%;
+     border: solid black;
+     font-size: x-large;
+ }
+ .card-header{
+     margin: 10%;
+ }
+ .card-body{
+     margin: 5%;
+ }
+</style>
+
+</head>
+
+<body>
+<div class=header>
+ <h1>My Team</h1>
+</div>
+<div class="container-body">
+   ${teamstr} 
+   </div>
+<script src="https://kit.fontawesome.com/257de25400.js" crossorigin="anonymous"></script>         
+</body>
+
+</html>`;
 };
 
-// generate cards for each employee by class
-
+//arr is the employee object and looking for the properties in that class
 const generateCard = function (arr) {
-  // change font awesome icons based on role
-
-  let positionIcon;
-
+  //if else statement
   let roleInfo;
 
   if (arr.title === "Manager") {
-    positionIcon = `<i class="fas fa-mug-hot"></i>`;
     roleInfo = `Office Number: ${arr.officeNumber}`;
   } else if (arr.title === "Engineer") {
-    positionIcon = `<i class="fas fa-glasses"></i>`;
-    roleInfo = `GitHub Username: <a href="https://github.com/${arr.github}" target="_blank">${arr.github}</a>`;
+    roleInfo = `Github Username: ${arr.github}`;
   } else if (arr.title === "Intern") {
-    positionIcon = `<i class="fas fa-user-graduate"></i>`;
     roleInfo = `School: ${arr.school}`;
   }
 
-  return `
-  <div class="col-md-4 col-sm-6 col-12 col-lg-3">
-    <div class="card shadow-lg mb-5 bg-white rounded">
-        <div class="card-header bg-primary">
-            <h4 class="text-white text-center">${arr.name}</h4>
-            <h4 class="text-white text-center">${positionIcon}</i> ${arr.title}</h4>
-        </div>
-        <div class="card-body">
-            <ul class="list-unstyled">
-                <li>Employee ID: ${arr.id}</li>
-                <li>Email: <a href="mailto:${arr.email}">${arr.email}</a></li>
-                <li>${roleInfo}</i>
-            </u>
-        </div>
-    </div>
-  </div>`;
+  return `<div class="card">
+<div class="card-header">
+<h2>${arr.name}</h2>  
+<h2><i class="far fa-user"></i> ${arr.title}</h2>
+<hr>
+</div>
+<div class="card-body">
+<ul>
+  <li>ID: ${arr.id}</li>
+  <li>Email: <a href="mailto:${arr.email}">${arr.email}</a></li>
+  <li>${roleInfo} </li>
+</ul>
+</div>
+</div>`;
 };
 
 exports.generateHTML = generateHTML;
