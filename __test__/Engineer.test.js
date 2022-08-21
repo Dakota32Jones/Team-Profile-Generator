6d2f3246ff75c329e60992c30f616b918a478940
Dakota Jones <dakota32jones@gmail.com>
2022-08-21 00:25:20 -0500

    added test method for engineer

---
+++ __test__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("making github with a constructor", () => {
+  const testValue = "whatever";
+  const employee = new Engineer("Dakota", 1, "test@gmail.com", testValue);
+  expect(employee.getRole()).toBe(testValue);
+});
+
+test("getRole function", () => {
+  const testValue = "Engineer";
+  const employee = new Engineer("Dakota", 1, "test@gmail.com", testValue);
+  expect(employee.getRole()).toBe(testValue);
+});
+
+test("github", () => {
+  const testValue = "whatever";
+  const employee = new Engineer("Dakota", 1, "test@gmail.com", testValue);
+  expect(employee.getGitHub()).toBe(testValue);
+});
